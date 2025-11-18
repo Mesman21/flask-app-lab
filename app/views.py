82b27f520a9d6314fbe35103956403b1bd31c86f
Mesman21 <149572864+Mesman21@users.noreply.github.com>
2025-11-18 18:41:29 +0200

    feat(lab5): final commit, completed all tasks including logging and validation

--- app/views.py
+++ app/views.py
@@ -1,10 +1,23 @@
+from flask import render_template, redirect, url_for, flash
 from app import app
-from flask import render_template
+from .forms import ContactForm
+import logging
 
-@app.route("/")
+logging.basicConfig(filename='contact.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+
+@app.route('/')
 def resume():
     return render_template("resume.html")
 
-@app.route("/contacts")
+
+@app.route('/contacts', methods=['GET', 'POST'])
 def contacts():
-    return render_template("contacts.html")
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        logging.info(f"Contact form submitted by Name: {form.name.data}, Email: {form.email.data}")
+        flash(f"Повідомлення від {form.name.data} ({form.email.data}) успішно надіслано!", 'success')
+        return redirect(url_for('contacts'))
+
+    return render_template('contacts.html', form=form)
